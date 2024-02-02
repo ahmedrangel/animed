57b3929e1acb8017b8109e60b0496689bfed91da
Ahmed Rangel <ahmedrangel@outlook.com>
2024-02-02 09:10:39 -0500

    fix: iframe video bugs

--- components/video/Modal.vue
+++ components/video/Modal.vue
@@ -3,17 +3,28 @@ const props = defineProps({
   id: { type: String, required: true },
   video: { type: String, required: true },
 });
+
+onMounted(() => {
+  const modal = document.querySelector("#" + props.id) as HTMLElement;
+  modal.addEventListener("click", (e: Event) => {
+    const target = e.target as Node;
+    const iframe = document.querySelector("#embed") as HTMLIFrameElement;
+    if (!iframe?.contains(target)) {
+      iframe.contentWindow?.postMessage("{\"event\":\"command\",\"func\":\"stopVideo\",\"args\":\"\"}", "*");
+    }
+  });
+});
 </script>
 
 <template>
   <div :id="props.id" class="modal fade w-100 p-0" tabindex="-1" role="dialog" :aria-labelledby="props.id" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-dialog modal-dialog-centered">
       <div class="modal-content">
         <div class="modal-body">
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close" />
           <!-- 16:9 aspect ratio -->
           <div class="ratio ratio-16x9">
-            <iframe width="1280" height="720" :src="props.video" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" />
+            <iframe id="embed" width="1280" height="720" :src="props.video + '?enablejsapi=1'" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture" />
           </div>
         </div>
       </div>
