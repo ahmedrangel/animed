d37ca03f510dbf423e2dd08563c4022e6fdbdcc0
Ahmed Rangel <ahmedrangel@outlook.com>
2024-02-06 18:44:37 -0500

    fix: null banners

--- components/component/Banner2.vue
+++ components/component/Banner2.vue
@@ -5,7 +5,7 @@ const props = defineProps({
 </script>
 
 <template>
-  <div class="banner p-0 position-relative d-flex align-items-center p-0 w-100 overflow-hidden border-bottom">
+  <div v-if="props.anime?.bannerImage" class="banner p-0 position-relative d-flex align-items-center p-0 w-100 overflow-hidden border-bottom">
     <span id="blur" class="position-absolute top-0 w-100 h-100 bg-secondary" :style="{ 'background-image': 'url(' + props.anime?.bannerImage + ')' }" />
     <div id="front" />
   </div>
