9032b000f83d4e9bcaa2bf72aa7419e9d8b59057
Ahmed Rangel <ahmedrangel@outlook.com>
2024-02-12 16:49:51 -0500

    chore: random trending anime on banner

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,17 @@
 <script setup lang="ts">
 const { data: data } = await useFetch("/api/explore/") as Record<string, any>;
-const popular = data.value.preview[data.value.preview.length - 1].data[0];
+const popular = useState("random-anime", () => null);
+
+const trendings = data.value.preview[data.value.preview.length - 1];
+
+if (!popular.value) {
+  popular.value = trendings.data[Math.floor(Math.random() * trendings.data.length - 1)];
+}
 </script>
 
 <template>
   <section id="preview">
-    <ComponentBanner :anime="popular" />
+    <ComponentBanner v-if="popular" :anime="popular" />
     <ComponentPreviewList :data="data" />
   </section>
 </template>
