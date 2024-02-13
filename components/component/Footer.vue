ac83e536c31d51d6a5e9a809484a8b057038159a
Ahmed Rangel <ahmedrangel@outlook.com>
2024-02-13 11:19:09 -0500

    chore: add github

--- components/component/Footer.vue
+++ components/component/Footer.vue
@@ -4,7 +4,7 @@
       <img src="/images/animed.svg" class="pe-none" height="40" :title="SITE.domain">&nbsp;&nbsp;
       <h5 class="mb-0">{{ SITE.domain.toUpperCase() }}</h5>
     </div>
-    <div class="text-muted">
+    <div class="text-muted mb-2">
       <div class="d-flex align-items-center mb-2">
         <h6 class="mb-0 fw-normal">Data provided by&nbsp;</h6>
         <a target="_blank" href="https://anilist.co">
@@ -13,5 +13,10 @@
       </div>
       <h6 class="mb-0 fw-normal">This project uses the AniList API but is not endorsed or certified by AniList.&nbsp;</h6>
     </div>
+    <a target="_blank" :href="SITE.github">
+      <h2 class="mb-0 text-muted">
+        <Icon name="simple-icons:github" />
+      </h2>
+    </a>
   </div>
 </template>
--- utils/info.ts
+++ utils/info.ts
@@ -2,4 +2,5 @@ const domain = "anime.ahmedrangel.com";
 export const SITE = {
   url: `https://${domain}`,
   domain,
+  github: "https://github.com/ahmedrangel/anime"
 };
