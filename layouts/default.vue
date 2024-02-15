b71181c4946f634f72779582515a0cb88cd94b2c
Ahmed Rangel <ahmedrangel@outlook.com>
2024-02-14 19:05:09 -0500

    fix: layouts

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,8 @@
 <template>
   <div id="layout" class="col px-0 pb-5 mb-4 pb-lg-0 mb-lg-0">
-    <Suspense>
-      <div>
-        <main>
-          <slot />
-        </main>
-        <ComponentFooter />
-      </div>
-    </Suspense>
+    <main>
+      <slot />
+    </main>
+    <ComponentFooter />
   </div>
 </template>
--- layouts/no-footer.vue
+++ layouts/no-footer.vue
@@ -1,11 +1,7 @@
 <template>
   <div id="layout" class="col px-0">
-    <Suspense>
-      <div>
-        <main>
-          <slot />
-        </main>
-      </div>
-    </Suspense>
+    <main>
+      <slot />
+    </main>
   </div>
 </template>
