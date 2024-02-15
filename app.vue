ca4174fc5547e98fa9aff98dbb1d0f99a9ab32e3
Ahmed Rangel <ahmedrangel@outlook.com>
2024-02-14 19:31:39 -0500

    chore: adjust transition

--- app.vue
+++ app.vue
@@ -14,10 +14,11 @@ setScrollBehavior();
 <style>
 .fade-enter-active,
 .fade-leave-active {
-  transition: opacity 0.15s ease;
+  transition: opacity 0.1s ease;
 }
 .fade-enter-from,
 .fade-leave-to {
   opacity: 0;
+  filter: blur(0.3rem);
 }
 </style>
