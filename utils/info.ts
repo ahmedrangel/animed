9b3977525118979e0ca1372718e8f890f80b3f2a
Ahmed Rangel <ahmedrangel@outlook.com>
2024-04-21 02:01:44 -0500

    chore: update domain and github link

--- utils/info.ts
+++ utils/info.ts
@@ -1,8 +1,8 @@
-const domain = "anime.ahmedrangel.com";
+const domain = "animed.ahmedrangel.com";
 export const SITE = {
   url: `https://${domain}`,
   domain,
-  github: "https://github.com/ahmedrangel/anime",
+  github: "https://github.com/ahmedrangel/animed",
   name: "Animed",
   og_card: "/images/og-card.png"
 };
