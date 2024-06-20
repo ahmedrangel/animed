90e5ae406032519aed62759d7ef85301770891a1
Ahmed Rangel <ahmedrangel@outlook.com>
2024-06-20 02:07:48 -0500

    fix: aflv response

--- utils/animeflv.client.ts
+++ utils/animeflv.client.ts
@@ -2,6 +2,6 @@ import { API } from "~/enums/animeflv-api";
 
 export const getAflvSearch = async (query: string) => {
   const response = await $fetch(API.BASE + "/search?query=" + query).catch((e) => console.info(e)) as Record<string, any>;
-  const data = response?.search?.data || [];
+  const data = response?.data?.media || [];
   return data;
 };
