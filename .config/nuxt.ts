186d8dc69ff9fdf9334b4069b89538e0e20a3322
Yizack Rangel <yizackr@gmail.com>
2024-06-19 22:28:59 -0500

    chore: lint

--- .config/nuxt.ts
+++ .config/nuxt.ts
@@ -17,14 +17,14 @@ export default defineNuxtConfig({
         { name: "robots", content: "index, follow" },
         { property: "og:ttl", content: "604800" }
       ],
-      link : [
+      link: [
         { rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png" },
         { rel: "icon", type: "image/png", sizes: "512x512", href: "/android-chrome-512x512.png" },
         { rel: "icon", type: "image/png", sizes: "192x192", href: "/android-chrome-192x192.png" },
         { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png" },
         { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon-16x16.png" },
         { rel: "manifest", href: "/site.webmanifest" },
-        { rel: "mask-icon", href: "/safari-pinned-tab.svg", color: "#0b0e0f" },
+        { rel: "mask-icon", href: "/safari-pinned-tab.svg", color: "#0b0e0f" }
       ]
     }
   },
@@ -97,7 +97,7 @@ export default defineNuxtConfig({
     display: "swap",
     download: true,
     families: {
-      "Rubik": [300, 400, 500, 600, 700],
-    },
+      Rubik: [300, 400, 500, 600, 700]
+    }
   }
 });
