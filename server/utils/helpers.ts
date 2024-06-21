68003c7021c09382ebd713dc701d7e00b73dbbf2
Ahmed Rangel <ahmedrangel@outlook.com>
2024-06-21 15:39:55 -0500

    chore: add GoogleOther to known bots

--- server/utils/helpers.ts
+++ server/utils/helpers.ts
@@ -30,7 +30,7 @@ export const animeFlvRelationLogic = (aflvArr: Record<string, any>[], anilistObj
 const RATE_LIMIT_MAX_REQ = 2;
 const RATE_LIMIT_TIME_FRAME = 1000 * 60 * 5;
 
-const knownBots = ["facebookexternalhit"];
+const knownBots = ["facebookexternalhit", "GoogleOther"];
 
 export const botRateLimitHandler = async (agent: string | undefined) => {
   const RATE_LIMIT_KV = process.env.ANIMED_BOT_RATE_LIMIT_BUCKET as any;
