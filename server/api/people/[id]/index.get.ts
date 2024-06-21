ec4ce522892130f782b00a0489c25795acb118ad
Ahmed Rangel <ahmedrangel@outlook.com>
2024-06-21 06:26:40 -0500

    chore: adjust

--- server/api/people/[id]/index.get.ts
+++ server/api/people/[id]/index.get.ts
@@ -20,7 +20,7 @@ export default defineEventHandler(async (event) => {
     }
   }
 
-  const userAgent = event.headers.get("user-agent");
+  const userAgent = cloudflare.req.headers.get("user-agent");
   const limited = await botRateLimitHandler(userAgent);
   if (limited)
     throw createError({ statusCode: 429, statusMessage: "Too many requests" });
