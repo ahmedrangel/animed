8733871429002e2a008883df1b387ddaadd92183
Ahmed Rangel <ahmedrangel@outlook.com>
2024-06-21 00:07:09 -0500

    chore: update sitemap

--- server/routes/__sitemap.ts
+++ server/routes/__sitemap.ts
@@ -5,7 +5,7 @@ export default defineEventHandler(() => {
   const routeRules = [] as Record<string, any>;
   for (const c of categories) {
     const slug = fixSlug(c.name);
-    const rules = [`/c/${slug}`, `/c/new/${slug}`, `/c/top-rated/${slug}`, `/c/trending/${slug}`];
+    const rules = [`/c/${slug}`, `/c/new/${slug}`, `/c/top-rated/${slug}`, `/c/trending/${slug}`, `/c/upcoming/${slug}`];
     for (const r of rules) {
       routeRules.push({ loc: r, lastmod: new Date().toISOString() });
     }
