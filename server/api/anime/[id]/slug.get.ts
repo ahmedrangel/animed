02f35c29b82ca7df3e699535dbb97348d1bbf950
Ahmed Rangel <ahmedrangel@outlook.com>
2024-06-21 22:04:31 -0500

    chore: use cached event handler function

--- server/api/anime/[id]/slug.get.ts
+++ server/api/anime/[id]/slug.get.ts
@@ -1,42 +1,10 @@
-export default defineEventHandler(async (event) => {
-  const { cloudflare } = event.context;
-  const { href: reqURL } = getRequestURL(event);
-  let cacheManager = {
-    cache: null as any,
-    cacheKey: null as Request | null
-  };
-
-  // Check cache
-  if (!import.meta.dev) {
-    const cacheKey = new Request(reqURL, cloudflare.req);
-    // @ts-expect-error
-    const cache = caches.default;
-    cacheManager = { cache, cacheKey };
-    const response = await cache.match(cacheKey);
-
-    if (response) {
-      console.info("Found in cache!");
-      return response;
-    }
-  }
-
+export default defineCachedEventHandler(async (event) => {
   const { id } = getRouterParams(event);
   const data = await getAnimeSlug(Number(id));
   const slug = fixSlug(data.Media.title.romaji);
   const obj = { id, slug };
 
-  const response = new Response(JSON.stringify(obj), {
-    headers: {
-      "Content-Type": "application/json;charset=UTF-8",
-      "Access-Control-Allow-Origin": "*",
-      "Cache-Control": "s-maxage=604800" // 1w cache
-    }
-  });
+  const response = obj;
 
-  if (data && !import.meta.dev) {
-    console.info("Stored in cache!");
-    cloudflare.context.waitUntil(cacheManager.cache.put(cacheManager.cacheKey, response.clone()));
-  }
-
-  return response;
-});
+  if (response) return response;
+}, { maxAge: !import.meta.dev ? 604800 : 0 }); // 1w cache
--- server/api/people/[id]/index.get.ts
+++ server/api/people/[id]/index.get.ts
@@ -1,25 +1,4 @@
-export default defineEventHandler(async (event) => {
-  const { cloudflare } = event.context;
-  const { href: reqURL } = getRequestURL(event);
-  let cacheManager = {
-    cache: null as any,
-    cacheKey: null as Request | null
-  };
-
-  // Check cache
-  if (!import.meta.dev) {
-    const cacheKey = new Request(reqURL, cloudflare.req);
-    // @ts-expect-error
-    const cache = caches.default;
-    cacheManager = { cache, cacheKey };
-    const response = await cache.match(cacheKey);
-
-    if (response) {
-      console.info("Found in cache!");
-      return response;
-    }
-  }
-
+export default defineCachedEventHandler(async (event) => {
   const userAgent = getHeaders(event)["user-agent"];
   const limited = await botRateLimitHandler(userAgent);
   if (limited)
@@ -29,18 +8,7 @@ export default defineEventHandler(async (event) => {
   const { data } = await getStaff({ id: Number(id) });
   const obj = data.Staff;
 
-  const response = new Response(JSON.stringify(obj), {
-    headers: {
-      "Content-Type": "application/json;charset=UTF-8",
-      "Access-Control-Allow-Origin": "*",
-      "Cache-Control": "s-maxage=604800" // 1w cache
-    }
-  });
+  const response = obj;
 
-  if (data && !import.meta.dev) {
-    console.info("Stored in cache!");
-    cloudflare.context.waitUntil(cacheManager.cache.put(cacheManager.cacheKey, response.clone()));
-  }
-
-  return response;
-});
+  if (response) return response;
+}, { maxAge: !import.meta.dev ? 43200 : 0 });// 12h cache
