8374be9a36b9db185237a512f92c279e2c2619a5
Ahmed Rangel <ahmedrangel@outlook.com>
2024-07-04 05:10:30 -0500

    chore: remove duplicated css

--- app.vue
+++ app.vue
@@ -11,14 +11,3 @@ setScrollBehavior();
     </NuxtLayout>
   </NavbarComp>
 </template>
-
-<style>
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.2s ease-in-out;
-}
-.fade-enter-from,
-.fade-leave-to {
-  opacity: 0;
-}
-</style>
