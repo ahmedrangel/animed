ac06d47ec8d4919e47d5ffbd88be30c98d3b27e5
Ahmed Rangel <ahmedrangel@outlook.com>
2024-08-16 04:21:24 -0500

    chore: preconnect anilist cdn

--- .config/nuxt.ts
+++ .config/nuxt.ts
@@ -28,7 +28,8 @@ export default defineNuxtConfig({
         { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png" },
         { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon-16x16.png" },
         { rel: "manifest", href: "/site.webmanifest" },
-        { rel: "mask-icon", href: "/safari-pinned-tab.svg", color: "#0b0e0f" }
+        { rel: "mask-icon", href: "/safari-pinned-tab.svg", color: "#0b0e0f" },
+        { rel: "preconnect", href: "https://s4.anilist.co" }
       ]
     }
   },
