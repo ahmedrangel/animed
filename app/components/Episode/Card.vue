2727e4b8868adcb6331b92b4c221ff3f09992d28
Ahmed Rangel <ahmedrangel@outlook.com>
2024-08-25 11:26:11 -0500

    fix: overflow on espisode card hover

--- app/components/Episode/Card.vue
+++ app/components/Episode/Card.vue
@@ -8,8 +8,8 @@ const ep = props.data;
 
 <template>
   <div class="h-100">
-    <a class="episode-card bg-secondary d-block rounded h-100 text-white" :href="ep.url" data-aos="fade-in">
-      <div class="bg-dark overflow-hidden position-relative rounded-bottom-0">
+    <a class="episode-card bg-secondary d-block rounded h-100 text-white overflow-hidden" :href="ep.url" data-aos="fade-in">
+      <div class="bg-dark position-relative rounded-bottom-0 overflow-hidden">
         <img class="img-fluid rounded-bottom-0 scale-on-hover w-100" :src="ep.thumbnail" :alt="ep.title" :title="ep.title">
         <span class="position-absolute top-50 start-50 translate-middle mb-0 invisible pe-none text-white"><Icon name="ci:external-link" /></span>
       </div>
