c9c5ecf07c21665e97a59e95a1b0268a3fadf614
Ahmed Rangel <ahmedrangel@outlook.com>
2024-10-03 00:26:52 -0500

    chore: add known bot

--- server/utils/helpers.ts
+++ server/utils/helpers.ts
@@ -25,7 +25,7 @@ export const animeFlvRelationLogic = (aflvArr: Record<string, any>[], anilistObj
 const RATE_LIMIT_MAX_REQ = 2;
 const RATE_LIMIT_TIME_FRAME = 1000 * 60 * 5;
 
-const knownBots = ["facebookexternalhit", "GoogleOther", "bingbot", "Googlebot", "GPTBot", "Bytespider", "SemrushBot", "Amazonbot", "meta-externalagent", "YandexBot", "ClaudeBot"];
+const knownBots = ["facebookexternalhit", "GoogleOther", "bingbot", "Googlebot", "GPTBot", "Bytespider", "SemrushBot", "Amazonbot", "meta-externalagent", "YandexBot", "ClaudeBot", "OAI-SearchBot"];
 
 export const botRateLimitHandler = async (agent: string | undefined) => {
   const RATE_LIMIT_KV = process.env.ANIMED_BOT_RATE_LIMIT_BUCKET as any;
