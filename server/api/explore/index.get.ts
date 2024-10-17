741f4347da3570cbe19d935b4649858ec7679685
Ahmed Rangel <ahmedrangel@outlook.com>
2024-10-17 07:56:48 -0500

    chore(experiment): always invalidate cache for testing purposes

--- server/api/explore/index.get.ts
+++ server/api/explore/index.get.ts
@@ -1,48 +1,28 @@
-export default defineEventHandler(async (event) => {
-  const { cloudflare } = event.context;
-  const { href: reqURL } = getRequestURL(event);
-  let cacheManager = {
-    cache: null as any,
-    cacheKey: null as Request | null
-  };
-
-  // Check cache
-  if (!import.meta.dev) {
-    const cacheKey = new Request(reqURL, cloudflare.req);
-    // @ts-expect-error
-    const cache = caches.default;
-    cacheManager = { cache, cacheKey };
-    const response = await cache.match(cacheKey);
-
-    if (response) {
-      const cacheBody = await response.clone().json();
-      if (!cacheBody.preview?.newly?.media?.length || !cacheBody.preview?.top?.media?.length || !cacheBody.preview?.trending?.media?.length) {
-        console.info("Cache cleared due to not matching required properties!");
-        cache.delete(cacheKey);
-      }
-      else {
-        console.info("Found in cache!");
-        return response;
-      }
-    }
-  }
-
+export default defineCachedEventHandler(async (event) => {
   const { slug } = getQuery(event) as { slug?: string };
   const cat_title = categories.find(c => fixSlug(c.name) === slug)?.name;
   const cat_type = categories.find(c => fixSlug(c.name) === slug)?.type || null;
   const option = slug ? cat_type === "genre" ? { genres: cat_title ? [cat_title] : null } : { tags: cat_title ? [cat_title] : null } : null;
-  const data = await getExplore({ ...option, slug, category: cat_title, perPage: 12 });
-  const response = new Response(JSON.stringify(data), {
-    headers: {
-      "Content-Type": "application/json;charset=UTF-8",
-      "Access-Control-Allow-Origin": "*",
-      "Cache-Control": "s-maxage=43200" // 12h cache
-    }
-  });
-
-  if (data && !import.meta.dev) {
-    console.info("Stored in cache!");
-    cloudflare.context.waitUntil(cacheManager.cache.put(cacheManager.cacheKey, response.clone()));
-  }
+  const response = await getExplore({ ...option, slug, category: cat_title, perPage: 12 });
   return response;
+}, {
+  maxAge: !import.meta.dev ? 43200 : 1,
+  shouldInvalidateCache: async (event) => {
+    if (!import.meta.dev) {
+      const { cloudflare } = event.context;
+      const { href: reqURL } = getRequestURL(event);
+      const cacheKey = new Request(reqURL, cloudflare.req);
+      // @ts-expect-error
+      const cache = caches.default;
+      const response = await cache.match(cacheKey);
+      if (response) {
+        const cacheBody = await response.clone().json();
+        if (!cacheBody.preview?.newly?.media?.length || !cacheBody.preview?.top?.media?.length || !cacheBody.preview?.trending?.media?.length) {
+          console.info("Cache invalidated due to not matching required properties!");
+          return true;
+        }
+      }
+    }
+    return false;
+  }
 });
