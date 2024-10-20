64f7b88592c5b622bf6e6dae10efc28cb512aa35
Ahmed Rangel <ahmedrangel@outlook.com>
2024-10-19 22:55:01 -0500

    fix: invalidate cache if response is incomplete

--- server/api/explore/index.get.ts
+++ server/api/explore/index.get.ts
@@ -8,20 +8,28 @@ export default defineCachedEventHandler(async (event) => {
 }, {
   maxAge: !import.meta.dev ? 43200 : 1,
   swr: true,
+  name: "getExplore",
+  getKey: (event) => {
+    const { slug } = getQuery(event) as { slug?: string };
+    return slug ? slug : "default";
+  },
   shouldInvalidateCache: async (event) => {
-    if (!import.meta.dev) {
-      const { cloudflare } = event.context;
-      const { href: reqURL } = getRequestURL(event);
-      const cacheKey = new Request(reqURL, cloudflare.req);
-      // @ts-expect-error
-      const cache = caches.default;
-      const response = await cache.match(cacheKey);
-      if (response) {
-        const cacheBody = await response.clone().json();
-        if (!cacheBody.preview?.newly?.media?.length || !cacheBody.preview?.top?.media?.length || !cacheBody.preview?.trending?.media?.length) {
-          console.info("Cache invalidated due to not matching required properties!");
-          return true;
-        }
+    const { slug } = getQuery(event) as { slug?: string };
+    const cacheKey = slug ? slug : "default";
+    const storageValue = `nitro:handlers:getExplore:${cacheKey}.json`;
+    const storage = await useStorage("cache").getItem(storageValue);
+    const cache = JSON.parse(JSON.stringify(storage));
+    const response = cache?.value?.body;
+    if (response) {
+      if (!response.preview?.newly?.media?.length || !response.preview?.top?.media?.length || !response.preview?.trending?.media?.length) {
+        console.info("Cache invalidated due to not matching required properties!");
+        event.node.res.setHeader("Cache-Control", "no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0");
+        event.node.res.setHeader("Pragma", "no-cache");
+        event.node.res.setHeader("Expires", "0");
+        event.node.res.setHeader("Surrogate-Control", "no-store");
+        const uniqueId = Date.now().toString();
+        event.node.res.setHeader("X-Response-ID", uniqueId);
+        return true;
       }
     }
     return false;
