1bb34525b3b7aaa8aafab358e2a348f0f0af9443
Ahmed Rangel <ahmedrangel@outlook.com>
2024-10-22 03:58:33 -0500

    chore: don't sacrifice loading times just for the video and set it client only

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,12 +1,7 @@
 <template>
   <div id="layout" class="col px-0 pb-5 mb-4 pb-lg-0 mb-lg-0">
     <div>
-      <Suspense>
-        <slot />
-        <template #fallaback>
-          <slot />
-        </template>
-      </Suspense>
+      <slot />
     </div>
     <FooterComp />
   </div>
