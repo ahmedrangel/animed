8d6cf569a14fae90cb13df17f85c127c142fa42c
Ahmed Rangel <ahmedrangel@outlook.com>
2024-11-05 09:17:07 -0500

    chore: add extra sitemap category

--- server/api/__sitemap.ts
+++ server/api/__sitemap.ts
@@ -1,6 +1,7 @@
 export default defineEventHandler(() => {
   const routeRules = [] as Record<string, any>;
-  for (const c of categories) {
+  const extraCategories = [{ name: "all" }];
+  for (const c of [...extraCategories, ...categories]) {
     const slug = fixSlug(c.name);
     const rules = [`/c/${slug}`, `/c/${slug}/new`, `/c/${slug}/top-rated`, `/c/${slug}/trending`, `/c/${slug}/upcoming`];
     for (const r of rules) {
