80801058b093a8d46c49e729edd5c242dcabbbf4
Ahmed Rangel <ahmedrangel@outlook.com>
2024-11-12 09:33:19 -0500

    chore: log bot name

--- shared/utils/composables.ts
+++ shared/utils/composables.ts
@@ -1,6 +1,9 @@
 export const useDetectCrawler = (option: { "user-agent"?: string }) => {
   const userAgent = option["user-agent"];
   const botName = [...limitedBots, ...unlimitedBots].find(bot => userAgent?.includes(bot));
-  if (botName) return { userAgent, isCrawler: true };
+  if (botName) {
+    console.info("Bot: " + botName);
+    return { userAgent, isCrawler: true };
+  }
   return { userAgent, isCrawler: false };
 };
