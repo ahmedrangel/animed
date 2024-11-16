f3310830c1197e9d57fb6ff4e1964386a2400e2d
Ahmed Rangel <ahmedrangel@outlook.com>
2024-11-16 00:20:16 -0500

    fix: navbar z-index

--- app/components/NavbarComp.vue
+++ app/components/NavbarComp.vue
@@ -30,7 +30,7 @@ const tabs = [
       </ul>
     </div>
     <slot />
-    <nav class="navbar bg-dark d-lg-none d-block border-top w-100 position-fixed bottom-0">
+    <nav class="navbar bg-dark d-lg-none d-block border-top w-100 position-fixed z-1 bottom-0">
       <ul id="menu" class="nav align-items-center justify-content-evenly">
         <li v-for="(tab, i) of tabs" :key="i" class="nav-item">
           <NuxtLink v-ripple :to="tab.route" class="nav-link p-2 text-body rounded" :title="tab.name">
