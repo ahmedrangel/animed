a58e59615bd4561c33ef08f69f5f865148e9e0dc
Ahmed Rangel <ahmedrangel@outlook.com>
2024-11-16 05:09:55 -0500

    feat: clear expired entries on mounted

--- app/plugins/idb.client.ts
+++ app/plugins/idb.client.ts
@@ -1,9 +1,15 @@
 import { createStorage, defineDriver } from "unstorage";
-import { get, set, clear, del, keys, createStore, type UseStore } from "idb-keyval";
+import { get, set, clear, del, keys, createStore, entries, type UseStore } from "idb-keyval";
 
 const DRIVER_NAME = "idb-keyval";
 
-export const indexedDbDriver = defineDriver((opts = {}) => {
+interface DriverOptions {
+  base: string;
+  dbName: string;
+  storeName: string;
+}
+
+export const indexedDbDriver = defineDriver((opts: DriverOptions) => {
   const base = opts.base && opts.base.length > 0 ? `${opts.base}:` : "";
   const makeKey = (key: string) => base + key;
   let customStore: UseStore | undefined;
@@ -20,6 +26,9 @@ export const indexedDbDriver = defineDriver((opts = {}) => {
     async getItem (key) {
       const item = await get(makeKey(key), customStore);
       return item ?? null;
+    },
+    async getEntries () {
+      return await entries(customStore);
     },
     setItem (key, value) {
       try {
@@ -41,21 +50,37 @@ export const indexedDbDriver = defineDriver((opts = {}) => {
   };
 });
 
-export default defineNuxtPlugin(() => {
+export default defineNuxtPlugin((nuxtApp) => {
+  const cache: DriverOptions = {
+    base: "app:",
+    dbName: "animed-cache",
+    storeName: "key-val"
+  };
+
+  const cacheExpiration: DriverOptions = {
+    base: "app:",
+    dbName: "animed-cache-expiration",
+    storeName: "key-val"
+  };
+
   const storage = createStorage({
-    driver: indexedDbDriver({
-      base: "app:",
-      dbName: "animed-cache",
-      storeName: "key-val"
-    })
+    driver: indexedDbDriver(cache)
   });
 
   const storageExpirations = createStorage({
-    driver: indexedDbDriver({
-      base: "app:",
-      dbName: "animed-cache-expiration",
-      storeName: "key-val"
-    })
+    driver: indexedDbDriver(cacheExpiration)
+  });
+
+  nuxtApp.hook("app:mounted", async () => {
+    const expirationStore = createStore(cacheExpiration.dbName, cacheExpiration.storeName);
+    const expirationEntries = await entries(expirationStore);
+    const now = new Date().getTime();
+    for (const [key, value] of expirationEntries) {
+      if (Number(value) < now) {
+        await useIdbStorage("cache").removeItem(String(key).replace(cacheExpiration.base, ""));
+        await useIdbStorage("expiration").removeItem(String(key).replace(cacheExpiration.base, ""));
+      }
+    }
   });
 
   return {
