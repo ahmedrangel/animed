c9c8dbdaf7482f65fe3deb1231a199e2f667e388
Ahmed Rangel <ahmedrangel@outlook.com>
2024-11-17 10:54:50 -0500

    chore: smooth transitions

--- app/components/Spinner/SpinnerFullScreenLoading.vue
+++ app/components/Spinner/SpinnerFullScreenLoading.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="d-flex justify-content-center align-items-center position-fixed z-3 h-100 bg-body" style="width: -webkit-fill-available;">
+  <div class="d-flex justify-content-center align-items-center position-fixed z-3 h-75" style="width: -webkit-fill-available;">
     <div class="text-muted spinner-border spinner-lg" role="status" style="width: 4rem; height: 4rem; font-size: 30px;">
       <span class="visually-hidden">Loading...</span>
     </div>
--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="layout" class="col d-flex flex-column px-0 mb-lg-0">
+  <div id="layout" class="col d-flex flex-column px-0 mb-lg-0 overflow-hidden position-relative">
     <div class="flex-grow-1">
       <slot />
     </div>
