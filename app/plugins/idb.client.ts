674b1071df4aa8f7a0527538041cecf79846204e
Ahmed Rangel <ahmedrangel@outlook.com>
2024-12-19 13:43:24 -0500

    chore: latest unstorage release support raw methods for indexed db

--- app/plugins/idb.client.ts
+++ app/plugins/idb.client.ts
@@ -1,80 +1,25 @@
-import { createStorage, defineDriver } from "unstorage";
-import { get, set, clear, del, keys, createStore, entries, type UseStore } from "idb-keyval";
-
-const DRIVER_NAME = "idb-keyval";
-
-interface DriverOptions {
-  base: string;
-  dbName: string;
-  storeName: string;
-}
-
-export const indexedDbDriver = defineDriver((opts: DriverOptions) => {
-  const base = opts.base && opts.base.length > 0 ? `${opts.base}:` : "";
-  const makeKey = (key: string) => base + key;
-  let customStore: UseStore | undefined;
-  if (opts.dbName && opts.storeName) {
-    customStore = createStore(opts.dbName, opts.storeName);
-  }
-  return {
-    name: DRIVER_NAME,
-    options: opts,
-    async hasItem (key) {
-      const item = await get(makeKey(key), customStore);
-      return item === void 0 ? false : true;
-    },
-    async getItem (key) {
-      const item = await get(makeKey(key), customStore);
-      return item ?? null;
-    },
-    async getItemRaw (key) {
-      const item = await get(makeKey(key), customStore);
-      return item ?? null;
-    },
-    async getEntries () {
-      return await entries(customStore);
-    },
-    setItem (key, value) {
-      return set(makeKey(key), value, customStore);
-    },
-    setItemRaw (key, value) {
-      return set(makeKey(key), value, customStore);
-    },
-    removeItem (key) {
-      return del(makeKey(key), customStore);
-    },
-    getKeys () {
-      return keys(customStore);
-    },
-    clear () {
-      return clear(customStore);
-    }
-  };
-});
+import { createStorage } from "unstorage";
+import indexedDbDriver, { type IDBKeyvalOptions } from "unstorage/drivers/indexedb";
+import { createStore, entries } from "idb-keyval";
 
 export default defineNuxtPlugin((nuxtApp) => {
-  const cache: DriverOptions = {
+  const cache: IDBKeyvalOptions = {
     base: "app:",
     dbName: "animed-cache",
     storeName: "key-val"
   };
 
-  const cacheExpiration: DriverOptions = {
+  const cacheExpiration: IDBKeyvalOptions = {
     base: "app:",
     dbName: "animed-cache-expiration",
     storeName: "key-val"
   };
 
-  const storage = createStorage({
-    driver: indexedDbDriver(cache)
-  });
-
-  const storageExpirations = createStorage({
-    driver: indexedDbDriver(cacheExpiration)
-  });
+  const storage = createStorage({ driver: indexedDbDriver(cache) });
+  const storageExpirations = createStorage({ driver: indexedDbDriver(cacheExpiration) });
 
   nuxtApp.hook("app:mounted", async () => {
-    const expirationStore = createStore(cacheExpiration.dbName, cacheExpiration.storeName);
+    const expirationStore = createStore(cacheExpiration.dbName!, cacheExpiration.storeName!);
     const expirationEntries = await entries(expirationStore);
     console.info("Cache enabled: IndexedDB");
     const now = new Date().getTime();
