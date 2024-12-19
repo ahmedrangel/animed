2d90c0a1f389e89d87fd7ade57ebc4bc9bb67e7e
Ahmed Rangel <ahmedrangel@outlook.com>
2024-12-19 13:46:58 -0500

    chore: lint

--- server/utils/helpers.ts
+++ server/utils/helpers.ts
@@ -1,5 +1,3 @@
-import type { H3Event } from "h3";
-
 const RATE_LIMIT_MAX_REQ = 2;
 const RATE_LIMIT_TIME_FRAME = 1000 * 60 * 5;
 
@@ -10,7 +8,7 @@ export const botRateLimitHandler = async (agent: string | undefined) => {
   const KV = hubKV();
   const now = Date.now() as number;
   const keyName = `RateLimitBucket:${botName}`.replace(/-/g, "");
-  const botRecord = await KV.get(keyName) as { count: number, lastReq: number };
+  const botRecord = await KV.get<{ count: number, lastReq: number }>(keyName);
   if (!botRecord) {
     await KV.set(keyName, { count: 1, lastReq: now });
     return false;
