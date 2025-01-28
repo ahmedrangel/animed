ad8e8aa07936ea6b7e6f3f9aa0e3d8b8c1d35036
Ahmed Rangel <ahmedrangel@outlook.com>
2025-01-28 10:24:19 -0500

    style: improve navbar button interaction styling

--- app/components/NavbarComp.vue
+++ app/components/NavbarComp.vue
@@ -31,20 +31,20 @@ const tabs: {
 
 <template>
   <div class="row m-0 flex-lg-row overflow-hidden min-vh-100">
-    <div class="sidebar p-0 bg-dark d-lg-block d-none border-end col-1">
+    <div class="sidebar p-0 d-lg-block d-none col-1">
       <ul id="menu" class="nav flex-column justify-content-evenly align-items-center min-vh-100 position-fixed z-3">
         <li v-for="(tab, i) of tabs" :key="i" class="nav-item w-100 d-flex flex-grow-1">
-          <NuxtLink v-ripple :to="tab.route" class="nav-link p-0 text-body d-flex justify-content-center align-items-center w-100" :title="tab.name">
+          <NuxtLink v-ripple :to="tab.route" class="nav-link p-0 text-body d-flex justify-content-center align-items-center w-100 border-end bg-dark" :title="tab.name">
             <h2 class="mb-0 d-flex align-items-center"><Icon :name="tab.icon" /></h2>
           </NuxtLink>
         </li>
       </ul>
     </div>
     <slot />
-    <nav class="navbar bg-dark d-lg-none d-block border-top w-100 position-fixed z-3 bottom-0 p-0">
+    <nav class="navbar d-lg-none d-block w-100 position-fixed z-3 bottom-0 p-0">
       <ul id="menu" class="nav align-items-center justify-content-evenly">
         <li v-for="(tab, i) of tabs" :key="i" class="nav-item flex-fill">
-          <NuxtLink v-ripple :to="tab.route" class="nav-link py-3 px-0 text-body" :title="tab.name">
+          <NuxtLink v-ripple :to="tab.route" class="nav-link py-3 px-0 text-body border-top bg-dark" :title="tab.name">
             <h2 class="mb-0 d-flex align-items-center justify-content-center"><Icon :name="tab.icon" /></h2>
           </NuxtLink>
         </li>
@@ -52,3 +52,12 @@ const tabs: {
     </nav>
   </div>
 </template>
+
+<style scoped>
+.nav-link:hover {
+  background-color: var(--bs-secondary)!important;
+}
+.nav-link.router-link-active {
+  background-color: var(--bs-body-bg)!important;
+}
+</style>
