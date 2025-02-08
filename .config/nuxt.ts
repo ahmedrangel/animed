95e06f7dc2f7d5a9090ab2793ee5cf34177d1db1
Ahmed Rangel <ahmedrangel@outlook.com>
2025-02-07 20:46:18 -0500

    Revert "chore: adjust nuxt config"
    This reverts commit a9601586bbc678806a34c8c17a3b27f4aaa9d382.

--- .config/nuxt.ts
+++ .config/nuxt.ts
@@ -2,7 +2,7 @@ import { SITE } from "../shared/utils/info";
 
 export default defineNuxtConfig({
   future: { compatibilityVersion: 4 },
-  compatibilityDate: "2025-02-07",
+  compatibilityDate: "2024-10-11",
   app: {
     pageTransition: { name: "fadepage", mode: "out-in" },
     layoutTransition: { name: "fadepage", mode: "out-in" },
@@ -116,6 +116,10 @@ export default defineNuxtConfig({
   },
   hub: {
     kv: true,
-    cache: true
+    cache: true,
+    bindings: {
+      compatibilityDate: "2024-10-20",
+      compatibilityFlags: ["nodejs_compat"]
+    }
   }
 });
