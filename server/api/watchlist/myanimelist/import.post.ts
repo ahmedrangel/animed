e75982f05bf1be1933cefa514b5b7a9ecf9cd4de
Ahmed Rangel <ahmedrangel@outlook.com>
2025-04-19 05:37:10 -0500

    fix: enhance watchlist import query perfomance

--- server/api/watchlist/myanimelist/import.post.ts
+++ server/api/watchlist/myanimelist/import.post.ts
@@ -12,10 +12,8 @@ export default defineEventHandler(async (event) => {
   const DB = useDB();
   const today = Date.now();
   await DB.delete(tables.watchList).where(eq(tables.watchList.userId, user.id)).run();
-  const returningList = [];
-  for (const item of body) {
-    const { mediaId, mediaSlug, progress, status, score, startedDate, finishedDate } = item;
-    const returning = await DB.insert(tables.watchList).values({
+  const mapValues = body.map(({ mediaId, mediaSlug, progress, status, score, startedDate, finishedDate }) => {
+    return {
       mediaId,
       mediaSlug,
       userId: user.id,
@@ -25,14 +23,24 @@ export default defineEventHandler(async (event) => {
       startedDate: startedDate || null,
       finishedDate: finishedDate || null,
       updatedAt: today
-    }).onConflictDoNothing().returning().get();
-    returningList.push(returning);
+    };
+  });
+  const chunkSize = 111;
+  let importedCount = 0;
+  const insertQuery = (values: string) => `INSERT INTO watch_list (media_id, media_slug, user_id, status, progress, score, started_date, finished_date, updated_at) VALUES ${values}`;
+  for (let i = 0; i < mapValues.length; i += chunkSize) {
+    const chunk = mapValues.slice(i, i + chunkSize);
+    const values = chunk.map(({ mediaId, mediaSlug, userId, status, progress, score, startedDate, finishedDate, updatedAt }) => {
+      return `(${mediaId}, '${mediaSlug}', ${userId}, ${status}, ${progress}, ${score}, ${startedDate ? `'${startedDate}'` : null}, ${finishedDate ? `'${finishedDate}'` : null}, ${updatedAt})`;
+    }).join(", ");
+    const returning = import.meta.dev ? await hubDatabase().exec(insertQuery(values)) : await event.context.cloudflare.env.DB.prepare(insertQuery(values)).all();
+    if (returning.count) importedCount = importedCount + chunk.length;
   }
-  if (!returningList.length) {
+  if (!importedCount) {
     throw createError({
       statusCode: 400,
       data: { success: false, message: "Failed to import watchlist" }
     });
   }
-  return { success: true, message: `Imported successfully ${returningList.length} items` };
+  return { success: true, message: `Imported successfully ${importedCount} items` };
 });
