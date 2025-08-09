9092ed873fd9e3358b6aed1a9faac24e591e5e8a
Ahmed Rangel <ahmedrangel@outlook.com>
2025-08-08 20:59:00 -0500

    chore: remove unused arg

--- app/utils/animeflv.ts
+++ app/utils/animeflv.ts
@@ -1,12 +1,8 @@
 import { API } from "~~/enums/animeflv-api";
 import { useCachedFetch } from "~~/shared/utils/composables";
 
-export const getAflvSearch = async (query: string, userAgent?: string): Promise<AFlvRequest["data"]["media"]> => {
-  const response = await useCachedFetch<AFlvRequest>(API.BASE + "/search?query=" + query, {
-    headers: {
-      ...userAgent ? { "User-Agent": userAgent } : {}
-    }
-  }, {
+export const getAflvSearch = async (query: string): Promise<AFlvRequest["data"]["media"]> => {
+  const response = await useCachedFetch<AFlvRequest>(API.BASE + "/search?query=" + query, {}, {
     cacheKey: `animeflv:${decodeURIComponent(query)?.replace(/\W/g, "")}`,
     swr: false,
     ttl: 86400
