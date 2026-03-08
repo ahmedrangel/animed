4111b4e90ea94341c5286b68524b941a22ded301
Ahmed Rangel <ahmedrangel@outlook.com>
2026-03-08 13:19:56 -0500

    chore: update deps

--- app/pages/a/[id]/[slug]/art.vue
+++ app/pages/a/[id]/[slug]/art.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import MasonryWall from "@yeger/vue-masonry-wall";
+import { MasonryWall } from "@yeger/vue-masonry-wall";
 
 const { lastRoute, currentRoute } = useNavigationRoute();
 const fromSameRoute = fromSameRouteParams(lastRoute, currentRoute);
