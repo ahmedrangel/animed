69aef2e94a4eea086265983fed1151828925c05f
Ahmed Rangel <ahmedrangel@outlook.com>
2026-04-05 15:35:08 -0500

    chore: add session max age

--- .config/nuxt.ts
+++ .config/nuxt.ts
@@ -72,6 +72,10 @@ export default defineNuxtConfig({
     },
     zerochan: {
       cookie: ""
+    },
+    session: {
+      password: "",
+      maxAge: 60 * 60 * 24 * 30 // 30 days
     }
   },
   routeRules: {
